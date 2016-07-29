ef67b22fad4d3af73670ac27a2c9b2b557eb3fa4
Eric Lorentzen <doctor.c.eze@gmail.com>
2016-07-28 22:55:40 -0500

    Update navbar.php

--- MAIN/components/navbar.php
+++ MAIN/components/navbar.php
@@ -12,7 +12,7 @@
                     foreach($_SESSION['CART'] as $product) {
                         $pCount += $product['Quantity'];
                     }
-            }
+                }
                 ?>
             <li><a href="cart.php">Cart<span id="product_count" class="badge"><?php echo $pCount; ?></span></a></li>
             <?php if(isset($auth) && $auth->check()) { ?>
@@ -24,69 +24,3 @@
         </ul>
     </div>
 </nav>
-
-<!--<div class="row">-->
-<!--    <div class="navbar navbar-fixed-top navbar-inverse">-->
-<!--        <div class="container-fluid">-->
-<!--            <!-- Brand and toggle get grouped for better mobile display -->
-<!--            <div class="navbar-header">-->
-<!--                <a class="navbar-brand" href="#">Nonsense Shopping</a>-->
-<!--            </div>-->
-<!---->
-<!--            <!-- Collect the nav links, forms, and other content for toggling -->
-<!--            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
-<!--                <ul class="nav navbar-nav">-->
-<!--                    <li class="active"><a href="#">Home</a></li>-->
-<!--                    <li><a href="#">Products</a></li>-->
-<!--                    <li><a href="#">Cart</a></li>-->
-<!--                    <li class="dropdown">-->
-<!--                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>-->
-<!--                        <ul class="dropdown-menu" role="menu">-->
-<!--                            <li><a href="#">Home</a></li>-->
-<!--                            <li><a href="#">Cart</a><i class="glyphicon glyphicon-cart"></i></li>-->
-<!--                            <li><a href="#">Something else here</a></li>-->
-<!--                            <li class="divider"></li>-->
-<!--                            <li><a href="#">Separated link</a></li>-->
-<!--                            <li class="divider"></li>-->
-<!--                            <li><a href="#">One more separated link</a></li>-->
-<!--                        </ul>-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!---->
-<!--                <!--            <form class="navbar-form navbar-left" role="search">-->
-<!--                <!--                <div class="form-group">-->
-<!--                <!--                    <input type="text" class="form-control" placeholder="Search">-->
-<!--                <!--                </div>-->
-<!--                <!--                <button type="submit" class="btn btn-default">Submit</button>-->
-<!--                <!--            </form>-->
-<!---->
-<!---->
-<!--                <!--     NAV-RIGHT-SIDE - Display either login or user info      -->
-<!--                <li class="dropdown navbar-right">-->
-<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <span class="caret"></span></a>-->
-<!--                    <ul class="dropdown-menu login" role="menu">-->
-<!---->
-<!--                        <li><h2>Login</h2></li>-->
-<!--                        <li>-->
-<!--                            <form id="login">-->
-<!--                                <div class="form-group">-->
-<!--                                    <label for="username" class="control-label">Username:</label>-->
-<!--                                    <input type="text" name="username" id="username" class="form-control">-->
-<!--                                </div>-->
-<!--                                <div class="form-group">-->
-<!--                                    <label for="password" class="control-label">Password</label>-->
-<!--                                    <input type="password" name="password" id="password" class="form-control">-->
-<!--                                </div>-->
-<!--                                <div class="form-group">-->
-<!--                                    <button type="submit" id="submit" class="login-submit btn btn-success">Login</button>-->
-<!--                                    <button type="reset" id="reset" class="login-submit btn btn-danger">Clear</button>-->
-<!--                                </div>-->
-<!--                            </form>-->
-<!--                        </li>-->
-<!--                    </ul>-->
-<!--                </li>-->
-<!--                <li class="pull-right"><a href="#">Cart</a><i class="glyphicon glyphicon-cart"></i></li>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
